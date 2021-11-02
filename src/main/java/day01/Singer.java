394c3aa6120f2127831fe067315d7a5c16ea7566
Anyunjong <dksdbswhd12@naver.com>
2021-11-02 13:57:29 +0900

    day01

---
+++ src/main/java/day01/Singer.java
@@ -0,0 +1,18 @@
+package day01;
+
+import java.util.List;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+public class Singer {	//vo, dto 객체들은 bean 으로 관리하지 않습니다. - Model객체
+	private int idx;
+	private String singer;
+	private List<String> albums;
+}
